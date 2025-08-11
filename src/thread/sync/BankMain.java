1e01a80b403d5fe2a30dad15489c494173ca5ca7
shingc <shingc@everon.co.kr>
2025-08-11 12:54:38 +0900

    동시성 문제에 대한 예시 코드
    메모리 가시성 문제를 떠나
    같은 인스턴스 값을 바라보며 시작한 두 Thread 의 작업이
    동시에 시작되었을 때
    volatile 로 메인 메모리의 값을 참조한다고 해도
    같은 문제가 발생
    
    검증 과정에서는 둘이 같은 값을 바라보고
    잔액에서 출금액을 빼는 로직이 시작되었기 때문
    따라서 같은 Account 를 바라보고 있을 때
    출금 요청과 같은 이벤트가 발생하면
    Lock 을 사용해야 할 것으로 보여짐

---
+++ src/thread/sync/BankMain.java
@@ -0,0 +1,27 @@
+package thread.sync;
+
+import static util.MyLogger.*;
+import static util.ThreadUtils.*;
+
+public class BankMain {
+
+    public static void main(String[] args) throws InterruptedException {
+        BankAccount account = new BankAccountV1(1000);
+
+        Thread t1 = new Thread(new WithdrawTask(account, 800), "t1");
+        Thread t2 = new Thread(new WithdrawTask(account, 800), "t2");
+
+        t1.start();
+        t1.join();
+
+        sleep(500);
+        log("t1 state : " + t1.getState());
+        log("t2 state : " + t1.getState());
+
+        t2.start();
+        t2.join();
+
+        log("최종 잔액 : " + account.getBalance());
+    }
+
+}
